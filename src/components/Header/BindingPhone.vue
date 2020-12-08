96aebca72add44a8abd2753c69602eedc68467fe
王君瑶 <wangjunyao@wangjunyaodeMacBook-Pro.local>
2020-12-08 20:56:21 +0800

    update

--- src/components/Header/BindingPhone.vue
+++ src/components/Header/BindingPhone.vue
@@ -62,7 +62,7 @@ export default {
       // 发送短信验证码
       const { success } = await this.axios({
         method: 'post',
-        url: 'https://api.hiifire.com/v1/tool/send_sms',
+        url: '/tool/send_sms',
         data: params,
         headers: { 'Content-Type': 'multipart/form-data' }
       })
@@ -87,7 +87,7 @@ export default {
       const { success } = await this.axios({
         method: 'post',
         url:
-          'https://api.hiifire.com/v1/user/bind?access-token=' +
+          '/user/bind?access-token=' +
           getCookie('wx-token').replace(/\"/g, ''),
         data: params,
         headers: { 'Content-Type': 'multipart/form-data' }
